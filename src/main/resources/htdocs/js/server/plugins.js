dc11489f2848272f855522b5976d388b7ee99f60
Nickardson <taylorgratzer@yahoo.com>
2014-05-03 17:30:04 -0400

    Cleaned up plugin interface, added restart button.

--- src/main/resources/htdocs/js/server/plugins.js
+++ src/main/resources/htdocs/js/server/plugins.js
@@ -6,18 +6,20 @@ Bukkit.pluginManager.plugins.forEach(function (plugin) {
     if (name == 'BukkitDebug') {
         table.push([
             name,
-            '<span class="label label-info">N/A</span>',
-            '<span class="label label-info">N/A</span>',
+            '<span class="label label-success">N/A</span>',
+            '<span class="label label-danger">N/A</span>',
+            '<span class="label label-primary">N/A</span>',
             '<span class="label label-info">N/A</span>'
         ])
     } else {
         table.push([
             name,
-            '<button data-plugin="' + name + '" class="btn btn-success plugin-enable">Enable</button>',
-            '<button data-plugin="' + name + '" class="btn btn-danger plugin-disable">Disable</button>',
-            '<button data-plugin="' + name + '" class="btn btn-info plugin-query">?</button>'
+            '<button title="Enable" data-plugin="' + name + '" class="btn btn-success plugin-enable"><span class="glyphicon glyphicon-ok"></span></button>',
+            '<button title="Disable" data-plugin="' + name + '" class="btn btn-danger plugin-disable"><span class="glyphicon glyphicon-remove"></span></button>',
+            '<button title="Reload" data-plugin="' + name + '" class="btn btn-primary plugin-reload"><span class="glyphicon glyphicon-refresh"></span></button>',
+            '<button title="Info" data-plugin="' + name + '" class="btn btn-info plugin-query"><span class="glyphicon glyphicon-question-sign"></span></button>'
         ]);
     }
 });
 
-writeTable(table, ["Name", "Enable", "Disable", "Check Enabled"]);
+writeTable(table);
