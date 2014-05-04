c2806e2f7efdcca5b1b5348af5e15aa0ffcbf4db
Nickardson <taylorgratzer@yahoo.com>
2014-05-03 22:43:41 -0400

    On large screens, split screen between code and output.

--- src/main/resources/htdocs/js/editor.js
+++ src/main/resources/htdocs/js/editor.js
@@ -2,29 +2,6 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
 
-$(document).ready(function(){
-    var heightUpdateFunction = function() {
-        // http://stackoverflow.com/questions/11584061/
-        var newHeight = (editor.getSession().getScreenLength() + 1)
-            * editor.renderer.lineHeight
-            + editor.renderer.scrollBar.getWidth();
-
-        $('#editor').height(newHeight.toString() + "px");
-        $('#editor-section').height(newHeight.toString() + "px");
-
-        // This call is required for the editor to fix all of
-        // its inner structure for adapting to a change in size
-        editor.resize();
-    };
-
-    // Set initial size to match initial content
-    heightUpdateFunction();
-
-    // Whenever a change happens inside the ACE editor, update
-    // the size again
-    editor.getSession().on('change', heightUpdateFunction);
-});
-
 editor.load = function(url){
     $.ajax({
         "url": url,
